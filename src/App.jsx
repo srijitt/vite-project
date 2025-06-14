92eb3d8b6c7a8b0cfd33529f0a384178f096df43
Souherdya Sarkar <souherdya.iema@gmail.com>
2025-06-14 20:56:54 +0530

    Name changed to Souherdya

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hi all, this is Srijit.</h1>
+      <h1>Hi all, this is Souherdya.</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
